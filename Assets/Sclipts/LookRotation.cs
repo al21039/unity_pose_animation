a781e41cf998d03f843c3a3058c832907446bd71
Rikuto Hara <130439038+al21039@users.noreply.github.com>
2024-07-10 19:10:27 +0900

    apply_model

--- Assets/Sclipts/LookRotation.cs
+++ Assets/Sclipts/LookRotation.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class LookRotation : MonoBehaviour
 {
-    public GameObject cube1;
-    public GameObject cube2;
+    public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +14,8 @@ public class LookRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var aim = cube1.transform.position - cube2.transform.position;
-        var look = Quaternion.LookRotation(aim, Vector3.up);
-        cube1.transform.rotation = look;
+        var aim = this.transform.position - target.transform.position;
+        var look = Quaternion.LookRotation(aim);
+        this.transform.rotation = look;
     }
 }
